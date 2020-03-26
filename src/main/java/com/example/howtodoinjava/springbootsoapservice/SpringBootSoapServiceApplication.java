f6aba09f1e255efd27f5ea7a5cbbe2f03d9f6878
DheepanRaj <maildheepan@gmail.com>
2020-03-26 10:37:58 +0530

    Springboot with Wildfly deployment
    Springboot with Wildfly deployment

---
+++ src/main/java/com/example/howtodoinjava/springbootsoapservice/SpringBootSoapServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.howtodoinjava.springbootsoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootSoapServiceApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapServiceApplication.class, args);
+	}
+
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+	    return builder.sources(SpringBootSoapServiceApplication.class);
+	  }
+}
